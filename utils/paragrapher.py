6a23759c8ac2be8e9e49b5fb69b321ffe1219bf2
jfmario <john.f.marion@gmail.com>
2015-09-27 15:08:12 +0000

    great doxology

--- utils/paragrapher.py
+++ utils/paragrapher.py
@@ -1,5 +1,6 @@
+import sys
 
 with open ( 'txt.txt', 'r' ) as f:
     ls = f.readlines()
     for line in ls:
-        print ( '<p>%s</p>' % line.strip () )
+        print ( '"%s",' % line.strip () )
