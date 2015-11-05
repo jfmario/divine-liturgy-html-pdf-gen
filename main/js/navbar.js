28080c5668b57ffe3ccc6c3b93a7586b4cbf9db2
jfmario <john.f.marion@gmail.com>
2015-11-05 21:26:08 +0000

    move export.html to index.html

--- main/js/navbar.js
+++ main/js/navbar.js
@@ -1,8 +1,7 @@
 
 var navMenu = [
     { href: 'index.html', text: 'Home' },
-    { href: 'settings.html', text: 'Settings' },
-    { href: 'export.html', text: 'Export' }
+    { href: 'settings.html', text: 'Settings' }
 ];
 
 BS_HELP.populateNavbar ( 'nav-ul', navMenu );
